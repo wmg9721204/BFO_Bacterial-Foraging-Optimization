430f31b0758c49fca8b308aed7f7c3a3a8cac89a
MIN CHUN WU <wmg9721204@gmail.com>
2018-12-05 17:06:16 -0500

    Modify/Imporve BFO
    1. Allow "Range" as an n x 2 Array. The pth row is the range for the pth variable of J.
    2. Make the bacterial paths stay in Range.

--- BFO.jl
+++ BFO.jl
@@ -1,5 +1,5 @@
 using LinearAlgebra ## "norm"
-using StatsBase ## for function "sample"
+using StatsBase ## "sample"
 """
 **BFO** is a function that computes the **classical (non-adaptive) BFO (Bacterial Foraging Optimization)**<br><br>
 **Inputs**:
@@ -11,8 +11,8 @@ using StatsBase ## for function "sample"
 5. **Sr** = 4,  number of bacteria removed in reproductive step
 6. **Nc** = 20, number of chemotactic steps
 7. **Ns** = 5, number of swim steps
-8. **Nre** = 10, number of reproductive steps
-9. **Ned** = 10, elimination and dispersal steps
+8. **Nre** = 5, number of reproductive steps
+9. **Ned** = 2, elimination and dispersal steps
 10. **Ped** = 0.3, probability of elimination
 11. **Ci** = (Range[2]-Range[1])/S, the run-length unit
 **Output**: a dictionary that stores
@@ -21,23 +21,36 @@ using StatsBase ## for function "sample"
 3. the path of each bacterium (for plotting illustration)
 """
 function BFO(J, Range; n = 2::Int, S = 10::Int, Sr = 4::Int, Nc = 20::Int, Ns = 5::Int, 
-        Nre = 10::Int, Ned = 10::Int, Ped = 0.3::Float64, Ci = ((Range[2]-Range[1])/S)::Float64)
-    ## randomly generate S bacteria in Range^n
-    B_loc = (Range[2]-Range[1])*rand(n,S).+Range[1] ## B_loc = Bacteria locations
+        Nre = 5::Int, Ned = 2::Int, Ped = 0.3::Float64, Ci = ((Range[2]-Range[1])/S)::Float64)
+    
+    ## randomly generate S bacteria in Range
+    Rand = rand(n,S) ## generate a random template
+    B_loc = zeros(n,S)  ## B_loc = Bacteria locations
+    for d = 1:n
+        B_loc[d,:] = (Range[d,2]-Range[d,1])*Rand[d,:].+Range[d,1]
+    end
+    B_loc
+    
     ## a dictionary recording the path of bacterium i
     Path_Dict = Dict(i=>[zeros(n,0) B_loc[:,i]] for i=1:S)
     for l = 1:Ned ## index of elimination-dispersal steps
         for k = 1:Nre ## index of reproductive steps
             for j = 1:Nc ## index of chemotactic steps
                 ## Chemotactic Step
+                println("Chemo-step $j")
                 for i = 1:S ## index of bacterium
                     ## Tumble/Swim
                     Path_i = copy(B_loc[:,i]) ## record the path of bacterium i
                     m = 0 ## counter for swimming
-                    delta_i = randn(n) ## random "tumble" direction (uniform on (n-1)-sphere)
                     while m<Ns
-                        J_last = J(B_loc[:,i]) ## last fitness value
+                        delta_i = randn(n) ## random "tumble" direction (uniform on (n-1)-sphere)
                         B_i_loc_new = B_loc[:,i] + Ci*delta_i/norm(delta_i) ## new location
+                        ## A while loop to guarantee the new bacterial position is in Range
+                        while sum((Range[:,1].<=B_i_loc_new).&(B_i_loc_new.<=Range[:,2]))!=n
+                            delta_i = randn(n) ## random "tumble" direction (uniform on (n-1)-sphere)
+                            B_i_loc_new = B_loc[:,i] + Ci*delta_i/norm(delta_i) ## new location
+                        end
+                        J_last = J(B_loc[:,i]) ## last fitness value
                         J_new = J(B_i_loc_new) ## new fitness value
                         if J_new<J_last ## swim
                             m = m+1
@@ -55,8 +68,10 @@ function BFO(J, Range; n = 2::Int, S = 10::Int, Sr = 4::Int, Nc = 20::Int, Ns = 
                     end
                 end
             end
+            
             if k<Nre
                 ## Reproductive Step
+                println("Repro-step $k")
                 ## (!!!) I define the health of a bacterium as the J value of its current location
                 Health = [J(B_loc[:,i]) for i=1:S]
                 Health_sort = sortslices([Health collect(1:S)], dims = 1) ## sort the bacteria according to health
@@ -73,16 +88,23 @@ function BFO(J, Range; n = 2::Int, S = 10::Int, Sr = 4::Int, Nc = 20::Int, Ns = 
             end
         end
         ## Elimination-Dispersal Step
+        println("ED-step $l")
         if l<Ned
             KillAlive = [sample([false, true], aweights([Ped,1-Ped])) for i=1:S] # true = alive, false = kill
             N_kill = S - sum(KillAlive) ## number of kiilled bacteria
             ## randomly generate a bacterium for each killed bacterium
             Alive = findall(KillAlive)
             Kill = setdiff(collect(1:S),Alive)
-            B_loc = sortslices([[Alive'; B_loc[:,Alive]] [Kill';(Range[2]-Range[1])*rand(n,length(Kill)).+Range[1]]], dims = 2)[2:n+1,:]
+            ## Rebuild the killed bacteria in random locations in Range
+            ED = [Alive'; B_loc[:,Alive]]
+            for dead in Kill
+                Revive_dead = [dead;[(Range[d,2]-Range[d,1])*rand(1)[1]+Range[d,1] for d=1:n]]
+                ED = [ED Revive_dead]
+            end
+            B_loc = sortslices(ED, dims = 2)[2:n+1,:]
             ## update the path dictionary
             for i = 1:S
-                if Path_Dict[i][:,end]!=B_loc[:,i]
+                if (Path_Dict[i][:,end])!=B_loc[:,i]
                     Path_Dict[i] = [Path_Dict[i] B_loc[:,i]]
                 end
             end
